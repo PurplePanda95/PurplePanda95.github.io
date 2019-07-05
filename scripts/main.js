b47b596d7f0c4b8d1f417038e3577a76ee3f65cb
PurplePanda95 <epneuwalder@gmail.com>
2019-07-05 17:56:19 -0400

    added jquery loader

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+(function() {
+    var startingTime = new Date().getTime();
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    // Poll for jQuery to come into existance
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 20);
+        }
+    };
+
+    // Start polling...
+    checkReady(function($) {
+        $(function() {
+            var endingTime = new Date().getTime();
+            var tookTime = endingTime - startingTime;
+            window.alert("jQuery is loaded, after " + tookTime + " milliseconds!");
+        });
+    });
+})();
 function get_json_content(request) {
     var content
     content = $.get(request+"&api_key=d50148cbfe594e27a232c50d1c2933a9", function() {
